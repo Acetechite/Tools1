8aa4892676b69b36833f5a6a72b321ca2fbd950c
Ryan <rytymedeiros@gmail.com>
2017-09-10 13:58:58 -0400

    Head Editor
    created an editor that lets you change child values for a multi part gameobject

---
+++ Tools 1/Assets/Scripts/CubeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CubeController : MonoBehaviour {
+
+	public Component[] facialComp;
+	public Vector3 myPosition;
+	public Transform head;
+	public float headScale = 1f;
+
+	void OnEnable(){
+		facialComp = GetComponentsInChildren<Transform> ();
+		foreach (Transform trans in facialComp) {
+			if (trans.tag == "Head")
+				head = trans;
+		}
+		Debug.Log (head.tag);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (myPosition.x, myPosition.y, myPosition.z);
+		head.localScale = new Vector3 (head.localScale.x, headScale, head.localScale.z);
+	}
+}
---
+++ Tools 1/Assets/Scripts/CubeControllerEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(CubeController))]
+public class CubeControllerEditor : Editor {
+	SerializedProperty myPosition;
+	SerializedProperty headScale;
+
+	void OnEnable(){
+		myPosition = serializedObject.FindProperty ("myPosition");
+		headScale = serializedObject.FindProperty ("headScale");
+	}
+
+	public override void OnInspectorGUI(){
+		serializedObject.Update ();
+		EditorGUILayout.PropertyField (myPosition);
+		EditorGUILayout.LabelField ("In put this here");
+		EditorGUILayout.Slider (headScale, 1f, 3f);
+		serializedObject.ApplyModifiedProperties ();
+	}
+}
